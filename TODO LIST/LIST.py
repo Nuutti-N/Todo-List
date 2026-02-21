ae0baeeda8f718bc38f1759e1ffe7ea7f0ebe143
Nuutti <nuutti.kyllonen.2006@gmail.com>
2026-02-21 11:58:54 +0200

    Add changes

--- TODO LIST/LIST.py
+++ TODO LIST/LIST.py
@@ -2,59 +2,59 @@
 
 
 import sys
-from colorama import Fore
+from colorama import Fore, Style
 import json
 
 
 def headlines():
 
-    print("=== TODO LIST ==")
+    print(Style.BRIGHT + Fore.YELLOW + "=== TODO LIST ==")
 
     return
 
 
 def read():
     try:
-        with open("data.json", "r") as f:
-            data = json.load(f)
-        print(data.items())
-        return data
+        with open("data.json", "r", encoding="UTF-8") as file:
+            data = json.load(file)
+        if isinstance(data, list):
+            return data
+        return []
     except FileNotFoundError:
         return []
 
 
 def menu():
-    print("Todo list, includes these functions.")
-    print("1. Add task")
-    print("2. Remove task")
-    print("3. Mark complete")
-    print("4. View all tasks")
-    print("0. Exit")
-    choose = int(input("Enter you choice: "))
+    print(Fore.WHITE + Style.BRIGHT + "Todo list, includes these functions.")
+    print(Fore.CYAN + "1. Add task")
+    print(Fore.CYAN + "2. Remove task")
+    print(Fore.CYAN + "3. Mark complete")
+    print(Fore.CYAN + "4. View all tasks")
+    print(Fore.CYAN + "0. Exit")
+    choose = int(input(Fore.YELLOW + "Enter you choice: "))
 
     return choose
 
 
 def add(Line):
-    Add = input("Enter add the task: ")
+    Add = input(Fore.CYAN + "Enter add the task: ")
     if Add == "":
-        print("Task cannot be empty!")
+        print(Fore.RED + "Task cannot be empty!")
     else:
         Line.append(Add)
-        print("✔ Task added!")
-
+        print(Fore.GREEN + "✔ Task added!")
     return Line
 
 
 def Remove(Line):
     try:
         # Firstly, asking what you want to remove, or nothing.
-        Remove = int(input("Enter Remove the number: "))
+        Remove = int(input(Fore.CYAN + "Enter Remove the number: "))
         if Remove == "":
-            print("Task cannot be empty!")
+            print(Fore.RED + "Task cannot be empty!")
         else:
             Line.pop(Remove - 1)
-            print("Task removed")
+            print(Fore.GREEN + "Task removed")
     except Exception:
         print("error, check the task")
 
@@ -62,48 +62,49 @@ def Remove(Line):
 
 
 def Mark(Line):
-    mark = int(input("Enter the task number to mark as done: "))
+    mark = int(input(Fore.CYAN + "Enter the task number to mark as done: "))
     if 0 <= mark < len(Line):
         Line[mark-1] = "✔ " + Line[mark-1]
-        print("Task marked done!")
+        print(Fore.GREEN + "Task marked done!")
     else:
-        print("Invalid task number.")
+        print(Fore.RED + "Invalid task number.")
     return Line
 
 
 def View(Line):
     if len(Line) == 0:
-        print("No Tasks yet! ✘")
+        print(Fore.RED + "No Tasks yet! ✘")
     else:
         for numbers, tasks in enumerate(Line, 1):
-            print("📋 Tasks!")
+            print(Fore.YELLOW + Style.BRIGHT + "📋 Tasks!")
             print(f"{numbers}. {tasks}")
 
     return Line
 
 
 def write(Line):
-    with open("data.json", "w") as f:
-        json.dump(Line, f)
+    with open("data.json", "w", encoding="UTF-8") as file:
+        json.dump(Line, file)
 
     return
 
 
 def main():
+    headlines()
     Line = read()
     write(Line)
     while True:
         choose = menu()
         if choose == 1:
-            add(Line)
+            Line = add(Line)
             write(Line)
             print()
         elif choose == 2:
-            Remove(Line)
+            Line = Remove(Line)
             write(Line)
             print()
         elif choose == 3:
-            Mark(Line)
+            Line = Mark(Line)
             write(Line)
             print()
         elif choose == 4:
@@ -111,9 +112,10 @@ def main():
             print()
         elif choose == 0:
             write(Line)
-            print("You regret the decision to EXIT!")
+            print(Fore.MAGENTA + "You regret the decision to EXIT!")
             break
-    print("Glad you completed your tasks for the day.")
+    print(Fore.GREEN + Style.BRIGHT +
+          "Glad you completed your tasks for the day.")
     return
 
 
